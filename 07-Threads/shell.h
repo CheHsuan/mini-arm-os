576b9707c24e7ea87e9a014903ed4b3bb4a5a16e
CheHsuan <exas60807@gmail.com>
2015-11-23 12:38:53 +0800

    Define LOCKED value

--- 07-Threads/shell.h
+++ 07-Threads/shell.h
@@ -1,9 +1,12 @@
 #define USART_SR_RXNE ((uint16_t) 0x0020)
 #define USART_FLAG_TXE  ((uint16_t) 0x0080)
 
+#define LOCKED 1
+
+void fib();
 void print_str(const char *str);
 void print_char(const char c);
 void print_int(int num);
-void commandCheck(const char *command);
+void commandCheck(const char *command,volatile int *a);
 int fibonacci(int num);
 void shell();
